e2c6f4d2c5381044faf597a7688626331f0a8198
Nicklas Vedsted <nicklasvedsted@gmail.com>
2021-12-12 19:22:45 +0100

    Add more circuits

--- benches/garbled_circuits.rs
+++ benches/garbled_circuits.rs
@@ -1,18 +1,26 @@
-use criterion::{BenchmarkId, Criterion, criterion_group, criterion_main};
+use criterion::{BenchmarkId, Criterion, criterion_group, criterion_main, Throughput};
 
+use cc_garbling::circuit::Circuit;
 use cc_garbling::garbled_circuit::{GarbledCircuit, GarbledEncoder};
 use cc_garbling::garbled_circuit::classic::Classic;
 use cc_garbling::garbled_circuit::half_gates::HalfGates;
 
-const INPUTS: [(&str, &str); 8] = [
+const INPUTS: [(&str, &str); 15] = [
     ("adder64", include_str!("../circuits/adder64.txt")),
     ("mult64", include_str!("../circuits/mult64.txt")),
     ("subtract64", include_str!("../circuits/subtract64.txt")),
     ("udivide64", include_str!("../circuits/udivide64.txt")),
     ("zero_equal", include_str!("../circuits/zero_equal.txt")),
     ("aes_128", include_str!("../circuits/aes_128.txt")),
+    ("aes_192", include_str!("../circuits/aes_192.txt")),
+    ("aes_256", include_str!("../circuits/aes_256.txt")),
     ("sha256", include_str!("../circuits/sha256.txt")),
     ("sha512", include_str!("../circuits/sha512.txt")),
+    ("Keccak_f", include_str!("../circuits/Keccak_f.txt")),
+    ("FP-add", include_str!("../circuits/FP-add.txt")),
+    ("FP-div", include_str!("../circuits/FP-div.txt")),
+    ("FP-mul", include_str!("../circuits/FP-mul.txt")),
+    ("FP-sqrt", include_str!("../circuits/FP-sqrt.txt")),
 ];
 
 macro_rules! garble {
@@ -28,7 +36,8 @@ macro_rules! garble {
 fn bench_garble(c: &mut Criterion) {
     let mut g = c.benchmark_group("garble");
     for (name, input) in INPUTS {
-        let circuit = input.parse().unwrap();
+        let circuit: Circuit = input.parse().unwrap();
+        g.throughput(Throughput::Elements((circuit.input_length + circuit.gates.len()) as u64));
         garble!(g, "HalfGates", HalfGates, name, circuit);
         garble!(g, "Classic", Classic, name, circuit);
     }
@@ -39,6 +48,7 @@ macro_rules! evaluate {
         for (name, input) in INPUTS {
             let circuit = input.parse().unwrap();
             let (gc, enc, _) = <$gc>::garble_circuit(&circuit);
+            $group.throughput(Throughput::Elements((circuit.input_length + circuit.gates.len()) as u64));
             let input = enc.encode(&vec![false; circuit.input_length]);
             $group.bench_with_input(
                 BenchmarkId::new($name, name), &input,
